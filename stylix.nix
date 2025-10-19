5f7389b66916e6f12ae43c981f1585edb865ba6d
moduzhou <moduzhou@qq.com>
2025-10-19 17:16:26 +0800

    add EdenQwQ's stylix.nix

---
+++ stylix.nix
@@ -0,0 +1,47 @@
+{
+  self,
+  lib,
+  hostname,
+  username,
+  ...
+}: let
+  cfg = self.homeConfigurations."${username}@${hostname}".config;
+in {
+  stylix =
+    {
+      enable = true;
+      autoEnable = false;
+      targets = {
+        console.enable = true;
+        plymouth.enable = true;
+      };
+    }
+    // (
+      if builtins.hasAttr "base16Scheme" cfg.stylix
+      then {base16Scheme = lib.mkDefault cfg.stylix.base16Scheme;}
+      else {}
+    )
+    // (
+      if builtins.hasAttr "image" cfg.stylix
+      then {image = lib.mkDefault cfg.stylix.image;}
+      else {}
+    );
+  specialisation =
+    builtins.mapAttrs (name: value: {
+      configuration = {
+        stylix =
+          (
+            if builtins.hasAttr "base16Scheme" value.configuration.stylix
+            then {base16Scheme = lib.mkForce cfg.specialisation.${name}.configuration.stylix.base16Scheme;}
+            else {}
+          )
+          // (
+            if builtins.hasAttr "image" value.configuration.stylix
+            then {image = lib.mkForce cfg.specialisation.${name}.configuration.stylix.image;}
+            else {}
+          );
+        environment.etc."specialisation".text = name;
+      };
+    })
+    cfg.specialisation;
+}
